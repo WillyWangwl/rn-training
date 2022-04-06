6604e1a6bb3a5f8833dbd163f621f63a4b460d77
WEILI WANG <weiliwang@WELIWANG-MacBook.local>
2022-04-06 19:13:00 +0800

    06 April 2022 Update v1

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -1,4 +1,4 @@
-import { TypeofTypeAnnotation } from "@babel/types";
+import { restElement, TypeofTypeAnnotation } from "@babel/types";
 import React from "react";
 import { Text, View } from "react-native";
 import styles from "./styles";
@@ -43,7 +43,7 @@ const Info: React.FC = () => {
   }
   greet(myName)
   // ---------------------------
-  // --------Anonymous Funtions---------
+  // --------Anonymous Functions---------
   const names: string[] = ["Alice", "Bob", "Eve"];
   names.forEach(function (s) {
     console.log(s.toUpperCase());
@@ -53,7 +53,7 @@ const Info: React.FC = () => {
   } )
   // ---------------------------
 
-  // --------objec types---------
+  // --------object types---------
   function printCoord(pt: { x:number; y:number}) {
     console.log(`x: ${pt.x}; y:${pt.y}`)
   }
@@ -92,6 +92,138 @@ const Info: React.FC = () => {
   day = DayOfTheWeek.MON;
   type dayToDisplay = keyof typeof DayOfTheWeek; 
   console.log(day);
+  // ---------------------------
+
+  // --------Literal Types---------
+  function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text'
+  ) {
+    let result;
+    if(typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
+      result  = +input1 + +input2;
+    } else {
+      result = input1.toString() + input2.toString()
+    }
+    return result;
+  }
+  console.log(combine(30,26,"as-number"));
+  console.log(combine("30","26","as-number"));
+  console.log(combine("Hello","World","as-text"));
+  // ---------------------------
+
+  // --------Narrowing---------
+  const formatErrorMessage = (value: string | Warning ): string => {
+    const prefix = "Error: ";
+    if ( typeof value === "string") {
+      return prefix + value;
+    }
+    if ( 'text' in value ) {
+      return prefix + value.text
+    }
+    return prefix;
+  } 
+  interface Warning {
+    text: string;
+  }
+  console.log(formatErrorMessage( {text: "What is the error"}));
+  // ---------------------------
+
+  // --------Function---------
+  function add(n1: number, n2:number) {
+    return n1 + n2;
+  }
+  function printResult(num: number): void {
+    console.log(`Result : ${num}`);
+  }
+  // call back function
+  function addAndHandle(n1:number, n2: number, cb: (num:number) => void ) {
+    const result = n1 + n2
+    cb(result);
+  }
+
+  printResult( add( 5 , 12 ) );
+  // let combineValues:Function;
+  let combineValues: ( a: number, b: number ) => number;
+  combineValues = add;
+  console.log( combineValues ( 8, 8 ) ); 
+  addAndHandle(10,30, (result) =>{
+    console.log(result);
+  })
+
+  const greeting = ( firstName: string, lastName: string ="James" ) : string => `Hello ${firstName} ${lastName}`;
+  
+  console.log(greeting("Willy"));
+  console.log(greeting("Willy", "Wang"))
+  
+  const combineAdd = <A,>(input1 : A, input2: A) : A => {
+    return input1 + input2;
+  }
+  console.log(combineAdd(30,26));
+  console.log(combineAdd("30","26"));
+  console.log(combineAdd("Hello"," World"));
+
+  type GreetFunction1 = ( name: string ) => string ;
+  const greeter: GreetFunction1 = (fn ) => {return  "hello " + fn ;}
+  type GreetFunction2 = ( name: string ) => void ;
+  const goodbye: GreetFunction2 = ( bye ) => console.log( "Bye " + bye );
+  console.log(greeter("John"))
+  goodbye("John");
+   // ---------------------------
+
+  // --------Call Signatures---------
+  type SignatureFunction = {
+    nameA : string;
+    ( isAdult: number ) : boolean;
+  };
+  const mySignatureX: SignatureFunction = ( age :number) => age > 18
+  mySignatureX.nameA = "Age is bigger than 18 ";
+  
+  const doSomething1 = ( fn: SignatureFunction ):void => {
+    console.log( fn.nameA + " : " + fn(18))
+  }
+  doSomething1(mySignatureX);
+  // ---------------------------
+
+  // --------REST Parameter---------
+  const printRest = (first: number, ...others: number[]) => {
+    console.log("first:", first);
+    console.log("rest:", others);
+  };
+  printRest(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  const array1 = [1, 2, 3]
+  const array2 = [...array1, 4 ,5]
+  console.log(array2)
+  // ---------------------------
+
+  // --------Parameter Destructuring---------
+  const displayDetail = ( { stdName, stdAge, stdAddress} : 
+    { stdName : string ; stdAge: number; stdAddress : string }):void => {
+      console.log(` Name: ${stdName}, Age: ${stdAge}, Address: ${stdAddress}`)
+  }
+  displayDetail({stdName: "willy",stdAge: 35,stdAddress: "Singapore"});
+
+  type NumberToSum = {
+    x: number;
+    y: number;
+    z: number;
+  }
+  const sum = ({ x, y, z }:NumberToSum): void => {
+    console.log(x + y + z);
+   }
+   const sumNumber = {x: 10, y: 20, z:30};
+   sum(sumNumber);
+   
+
+
+ 
+
+
+  
+
+
+
 
 
 
